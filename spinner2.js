cae60388065a0cf21de4fa471bc3ace96f620602
sachieko <kirazetsubou@gmail.com>
2022-08-02 15:56:40 -0600

    Refactored into spinner2

--- spinner2.js
+++ spinner2.js
@@ -1,26 +1,11 @@
-process.stdout.write('    hello from spinner1.js...');
-// Added intervals since after the start each animation repeats in 800ms.
-setTimeout(() => {
-  setInterval(() => {
-    process.stdout.write('\r|   ');
-  }, 800);
-}, 100);
-
-setTimeout(() => {
-  setInterval(() => {
-    process.stdout.write('\r/   ');
-  }, 800);
-}, 300);
-
-setTimeout(() => {
-  setInterval(() => {
-    process.stdout.write('\r-   ');
-  }, 800);
-}, 500);
-
-setTimeout(() => {
-  // Need to escape the backslash since it's a special character.
-  setInterval(() => {
-    process.stdout.write('\r\\   ');
-  }, 800);
-}, 700);
+const spinner = () => {
+  let animation = ['|', '/', '-', '\\'], delay = 100;
+  for (const char of animation) {
+    setTimeout(() => {
+      setInterval(() => {
+        process.stdout.write(`\r${char}   `);
+      }, 800);
+    }, delay += 200);
+  }
+};
+spinner();
